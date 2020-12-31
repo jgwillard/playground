83f50203952940c5c729d856f9b429ea5793cd0b
John Willard <jgwil2@gmail.com>
2020-12-31 17:16:02 -0500

    add template for problem files

---
+++ _problem_template.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class Solution:
+    def methodName(self):
+        pass
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def testMethodName(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
