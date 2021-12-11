6496dbb44b3f5eae003987d40d7a8d10e29b0479
John Willard <jgwil2@gmail.com>
2021-12-11 00:49:08 -0500

    add bottom up solution for max score problem

--- problems/max_score_mult_operations.py
+++ problems/max_score_mult_operations.py
@@ -56,6 +56,35 @@ class Solution:
 
         return dp(0, 0)
 
+    def maximumScoreBottomUp(self, nums: List[int], multipliers: List[int]) -> int:
+        n = len(nums)
+        m = len(multipliers)
+
+        dp: Dict[int, Dict[int, int]] = {i: {} for i in range(0, m + 1)}
+
+        # base case: explicitly set row m to have all 0 values, because
+        # on the first iteration of the loop where i == m - 1, we check
+        # each value of dp[i - 1] == dp[m], in which case there are no
+        # more operations so nothing to add to the score
+        dp[m] = {i: 0 for i in range(0, m + 1)}
+
+        for i in reversed(range(0, m)):
+            # in an iteration i, the left index has a maximum of i
+            # possible values if we always choose the leftmost number
+            # (the left index can increase by at most 1 per iteration)
+            # this loop iterates over all possible left-index values for
+            # the current value of i
+            for left in reversed(range(0, i + 1)):
+
+                right = n - 1 - (i - left)
+
+                dp[i][left] = max(
+                    multipliers[i] * nums[left] + dp[i + 1][left + 1],
+                    multipliers[i] * nums[right] + dp[i + 1][left],
+                )
+
+        return dp[0][0]
+
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -76,10 +105,18 @@ class TestSolution(unittest.TestCase):
             102,
         )
 
+    def testMaximumScoreBottomUp(self):
+        self.assertEqual(self.sol.maximumScoreBottomUp([1, 2, 3], [3, 2, 1]), 14)
+        self.assertEqual(
+            self.sol.maximumScoreBottomUp([-5, -3, -3, -2, 7, 1], [-10, -5, 3, 4, 6]),
+            102,
+        )
+
 
 if __name__ == "__main__":
     sol = Solution()
-    # NOTE manual memo beats lru_cache handily
+    # NOTE manual memo beats lru_cache handily and bottom up beats both
     print(timeit(lambda: sol.maximumScore([1, 2, 3], [3, 2, 1])), 1000)
     print(timeit(lambda: sol.maximumScoreAutoMemoized([1, 2, 3], [3, 2, 1])), 1000)
+    print(timeit(lambda: sol.maximumScoreBottomUp([1, 2, 3], [3, 2, 1])), 1000)
     unittest.main()
