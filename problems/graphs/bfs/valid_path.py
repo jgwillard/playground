82fb8623bb0b592b1ba6c4d7edefa5af2fcdef19
John Willard <jgwil2@gmail.com>
2022-03-23 22:20:03 -0400

    update valid path test cases, add bfs version

--- problems/graphs/bfs/valid_path.py
+++ problems/graphs/bfs/valid_path.py
@@ -1,4 +1,5 @@
-from typing import Dict, List
+from typing import Deque, Dict, List
+from collections import deque
 import unittest
 
 
@@ -15,13 +16,14 @@ class Solution:
             adjacency_list[edge[0]].append(edge[1])
             adjacency_list[edge[1]].append(edge[0])
 
-        # dfs
+        # bfs
         processed = [False] * n
-        vertex_stack: List[int] = [source]
+        vertex_queue: Deque[int] = deque(maxlen=n)
+        vertex_queue.append(source)
 
-        while vertex_stack:
+        while vertex_queue:
             # get current node
-            node = vertex_stack.pop()
+            node = vertex_queue.popleft()
 
             # check if we have reached destination
             if node == destination:
@@ -30,10 +32,10 @@ class Solution:
             if processed[node]:
                 continue
 
-            # add adjacent unprocessed nodes to stack
+            # add adjacent unprocessed nodes to queue
             for v in adjacency_list[node]:
                 if not processed[v]:
-                    vertex_stack.append(v)
+                    vertex_queue.append(v)
 
             # mark current node as processed
             processed[node] = True
@@ -50,6 +52,12 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.sol.validPath(3, [[0, 1], [1, 2], [2, 0]], 0, 2), True)
         self.assertEqual(
             self.sol.validPath(6, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0, 5), False
+        )
+        self.assertEqual(
+            self.sol.validPath(
+                6, [[0, 1], [0, 2], [2, 3], [3, 5], [5, 4], [4, 3]], 0, 5
+            ),
+            True,
         )
         self.assertEqual(
             self.sol.validPath(
