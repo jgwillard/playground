c5c4a07aef55bccfa2eb9abf6c979673cb19a542
John Willard <jgwil2@gmail.com>
2022-05-18 23:32:48 -0400

    implement min priority queue for prim's algorithm

--- problems/graphs/mst/min_cost_connect_points_prims.py
+++ problems/graphs/mst/min_cost_connect_points_prims.py
@@ -1,9 +1,49 @@
-from typing import List
+from typing import List, Optional
 import unittest
 
 
-class PriorityQueue(object):
-    pass
+class MinPriorityQueue(object):
+    def __init__(self):
+        self.heap: List[int] = [0]
+        self.count = 0
+
+    def isEmpty(self) -> bool:
+        return self.count == 0
+
+    def _swap(self, i: int, j: int):
+        temp = self.heap[i]
+        self.heap[i] = self.heap[j]
+        self.heap[j] = temp
+
+    def _swim(self, k: int):
+        while k > 1 and self.heap[k] < self.heap[k // 2]:
+            self._swap(k, k // 2)
+            k = k // 2
+
+    def insert(self, x: int):
+        self.count += 1
+        self.heap.append(x)
+        self._swim(self.count)
+
+    def _sink(self, k: int):
+        while k < self.count:
+            j = 2 * k
+            if j + 1 <= self.count and self.heap[j] > self.heap[j + 1]:
+                j += 1
+            if j <= self.count and not self.heap[k] < self.heap[j]:
+                self._swap(j, k)
+                k = j
+            else:
+                break
+
+    def removeMin(self) -> Optional[int]:
+        if self.isEmpty():
+            return None
+        self._swap(1, self.count)
+        self.count -= 1
+        top = self.heap.pop()
+        self._sink(1)
+        return top
 
 
 class Solution:
