226fa79dcc9539d14cb08dc4c14f60dd2811703c
John Willard <jgwil2@gmail.com>
2022-06-20 23:42:32 -0400

    handle unreachable nodes, clean up comments

--- problems/graphs/shortest_path/network_delay_time.py
+++ problems/graphs/shortest_path/network_delay_time.py
@@ -13,6 +13,10 @@ class Solution:
             else:
                 adjacency_list[source] = [(target, weight)]
 
+        # track whether all nodes are reachable from k
+        discovered = [False] * (n + 1)
+
+        # dijkstra's algorithm
         in_tree = [False] * (n + 1)
         dist = [MAX_INT] * (n + 1)
 
@@ -24,18 +28,20 @@ class Solution:
         while not in_tree[v]:
             # we are adding v to the tree
             in_tree[v] = True
+            discovered[v] = True
 
-            # i is a node adjacent to v
-            # we record the shortest distance from k to i by checking if
-            # the weight of the edge v->i plus the distance from k to v
-            # is less than the current shortest distance from k to i
-            #
-            # if it is, then we update the shortest distance from k to i
-            # to be equal to the weight of v->i plus dist from k to v
             if v in adjacency_list:
                 for edge in adjacency_list[v]:
+                    # i is a node adjacent to v
                     i, weight = edge
+                    # we record the shortest distance from k to i by
+                    # checking if the weight of the edge v->i plus the
+                    # distance from k to v is less than the current
+                    # shortest distance from k to i
                     if dist[v] + weight < dist[i]:
+                        # if it is, then we update the shortest distance
+                        # from k to i to be equal to the weight of v->i
+                        # plus dist from k to v
                         dist[i] = dist[v] + weight
 
             # we then add a node to the tree by iterating through all
@@ -52,6 +58,12 @@ class Solution:
                     min_dist = dist[i]
                     v = i
 
+        # if not all nodes are reachable, return -1
+        for i in range(1, n):
+            if not discovered[i]:
+                return -1
+
+        # if all nodes are reachable return the farthest distance from k
         head, *tail = dist
         return max(tail)
 
@@ -65,7 +77,7 @@ class TestSolution(unittest.TestCase):
             self.sol.networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2), 2
         )
         self.assertEqual(self.sol.networkDelayTime([[1, 2, 1]], 2, 1), 1)
-        # self.assertEqual(self.sol.networkDelayTime([[1, 2, 1]], 2, 2), -1)
+        self.assertEqual(self.sol.networkDelayTime([[1, 2, 1]], 2, 2), -1)
 
 
 if __name__ == "__main__":
