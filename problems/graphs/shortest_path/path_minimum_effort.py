3ec36bb96993a05d6282b4a0f14421c405b8c8cf
John Willard <jgwil2@gmail.com>
2022-09-29 22:04:08 -0400

    pseudocode out dijkstra for minimum effort path

--- problems/graphs/shortest_path/path_minimum_effort.py
+++ problems/graphs/shortest_path/path_minimum_effort.py
@@ -9,7 +9,30 @@ class Solution:
         num_rows = len(heights[0])
         # record minimum effort required to reach each cell
         minimum_effort: List[List[float]] = [[math.inf] * num_rows] * num_cols
-        visited: List[List[bool]] = [[False] * num_rows] * num_cols
+        in_tree: List[List[bool]] = [[False] * num_rows] * num_cols
+
+        x, y = (0, 0)
+        minimum_effort[0][0] = 0
+        # Dijkstra's algorithm
+        while in_tree[x][y] == False:
+            in_tree[0][0] = True
+            # go through all nodes adjacent to the tree and update their
+            # shortest distance to the tree (all have inf by default)
+
+            # for each node adjacent to v:
+            # weight = abs(heights[x][y] - heights[node_x][node_y])
+            #   if dist[v] + weight < dist[node]:
+            #       dist[node] = dist[v] + weight
+
+            # go through all the nodes not in the tree and add the one
+            # with the shortest distance to the tree
+
+            # dist = math.inf
+            # for i in range(num_rows):
+            #   for j in range(num_cols):
+            #       if not in_tree[i][j] and dist[i][j] < dist:
+            #           dist = dist[i][j]
+            #           x, y = i, j
 
         return int(minimum_effort[num_rows - 1][num_cols - 1])
 
