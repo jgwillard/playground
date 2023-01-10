94a1ccef91e1b3855f8ca2ca44886e3afac4eeef
John Willard <willard.john.gray@gmail.com>
2023-01-09 20:20:27 -0500

    partial path min effort solution w/ dijkstra's

--- problems/graphs/shortest_path/path_minimum_effort.py
+++ problems/graphs/shortest_path/path_minimum_effort.py
@@ -1,40 +1,65 @@
 import math
-from typing import List
+from typing import List, Tuple
 import unittest
 
 
 class Solution:
+    @staticmethod
+    def _get_adjacent_nodes(
+        x: int, y: int, num_cols: int, num_rows: int
+    ) -> List[Tuple[int, int]]:
+        nodes: List[Tuple[int, int]] = []
+        if x > 0:
+            nodes.append((x - 1, y))
+        if y > 0:
+            nodes.append((x, y - 1))
+        if x < num_rows - 1:
+            nodes.append((x + 1, y))
+        if y < num_cols - 1:
+            nodes.append((x, y + 1))
+        return nodes
+
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         num_cols = len(heights)
         num_rows = len(heights[0])
         # record minimum effort required to reach each cell
-        minimum_effort: List[List[float]] = [[math.inf] * num_rows] * num_cols
-        in_tree: List[List[bool]] = [[False] * num_rows] * num_cols
+        min_effort: List[List[float]] = [
+            [math.inf for j in range(num_cols)] for i in range(num_rows)
+        ]
+        in_tree: List[List[bool]] = [
+            [False for j in range(num_cols)] for i in range(num_rows)
+        ]
 
         x, y = (0, 0)
-        minimum_effort[0][0] = 0
+        min_effort[0][0] = 0
+
         # Dijkstra's algorithm
         while in_tree[x][y] == False:
-            in_tree[0][0] = True
-            # go through all nodes adjacent to the tree and update their
-            # shortest distance to the tree (all have inf by default)
-
-            # for each node adjacent to v:
-            # weight = abs(heights[x][y] - heights[node_x][node_y])
-            #   if dist[v] + weight < dist[node]:
-            #       dist[node] = dist[v] + weight
+            # mark curent node as visited and iterate over adjacent nodes
+            in_tree[x][y] = True
+            adjacent_nodes = self._get_adjacent_nodes(x, y, num_cols, num_rows)
+            for adj_x, adj_y in adjacent_nodes:
+                weight = abs(heights[x][y] - heights[adj_x][adj_y])
+                # min_effort is the maximum absolute difference in
+                # heights between two consecutive cells
+                # therefore, we just want the max of the currently
+                # recorded maximum height on this path and the current
+                # difference (weight)
+                if min_effort[adj_x][adj_y] > min_effort[x][y] + weight:
+                    min_effort[adj_x][adj_y] = min_effort[x][y] + weight
 
             # go through all the nodes not in the tree and add the one
-            # with the shortest distance to the tree
+            # with the shortest distance to the tree (can be sped up by
+            # priority queue)
+            dist: float = math.inf
+            for i in range(num_rows):
+                for j in range(num_cols):
+                    if not in_tree[i][j] and min_effort[i][j] <= dist:
+                        dist = min_effort[i][j]
+                        x, y = i, j
 
-            # dist = math.inf
-            # for i in range(num_rows):
-            #   for j in range(num_cols):
-            #       if not in_tree[i][j] and dist[i][j] < dist:
-            #           dist = dist[i][j]
-            #           x, y = i, j
-
-        return int(minimum_effort[num_rows - 1][num_cols - 1])
+        print(min_effort)
+        return int(min_effort[num_rows - 1][num_cols - 1])
 
 
 class TestSolution(unittest.TestCase):
