f6d3990a33c7daf3e920abef5be778855c47fa23
John Willard <willard.john.gray@gmail.com>
2023-02-07 22:04:07 -0500

    more progress on min height trees

--- problems/graphs/topo_sort/min_height_trees.py
+++ problems/graphs/topo_sort/min_height_trees.py
@@ -1,10 +1,38 @@
-from typing import List
+from typing import Dict, List, Deque
+from collections import deque
 import unittest
 
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        pass
+        adjacency_list: Dict[int, List[int]] = [[] for _ in range(n)]
+        indegree: List[int] = [0 for _ in range(n)]
+        leaf_nodes_queue: Deque[int] = deque()
+
+        for a, b in edges:
+            adjacency_list[a].append(b)
+            adjacency_list[b].append(a)
+
+        for _, neighbors in enumerate(adjacency_list):
+            for neighbor in neighbors:
+                indegree[neighbor] += 1
+
+        for node, _ in enumerate(adjacency_list):
+            if indegree[node] == 1:
+                leaf_nodes_queue.append(node)
+
+        while leaf_nodes_queue:
+            node = leaf_nodes_queue.popleft()
+            for neighbor in adjacency_list[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 1:
+                    leaf_nodes_queue.append(neighbor)
+
+            del adjacency_list[node]
+
+            # TODO check if the two nodes in adjacency list point to each other
+            if len(adjacency_list) == 2:
+                return [node for node, _ in enumerate(adjacency_list)]
 
 
 class TestSolution(unittest.TestCase):
