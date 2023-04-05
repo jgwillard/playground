1c6a310ec15e44b22d9630ad0b98cdea7857abd2
John Willard <willard.john.gray@gmail.com>
2023-04-04 22:06:20 -0400

    progress on eval division

--- problems/graphs/disjoint_set/evaluate_division.py
+++ problems/graphs/disjoint_set/evaluate_division.py
@@ -1,30 +1,57 @@
+import copy
 from typing import Dict, List
 import unittest
 
 
 class Solution:
-    solutions: List[List[str]] = []
-    graph: Dict[str, Dict[str, float]] = {}
-    visited: Dict[str, bool] = {}
     finished = False
 
-    def backtrack_dfs(self, path: List[str], dest: str):
+    def backtrack_dfs(
+        self,
+        path: List[str],
+        dest: str,
+        graph: Dict[str, Dict[str, float]],
+        solutions: List[List[str]],
+        visited: Dict[str, bool],
+    ):
+        if dest not in graph:
+            solutions.append([])
+            return
         item = path[-1]
         if item == dest:
-            self.solutions.append(path)
+            solutions.append(copy.copy(path))
             self.finished = True
+            return
         else:
-            for k, candidate in enumerate(self.graph[item]):
-                if not self.visited[candidate]:
+            for candidate in graph[item]:
+                if not visited[candidate]:
                     path.append(candidate)
-                    self.visited[candidate] = True
-                    self.backtrack_dfs(path, dest)
-                    self.visited[candidate] = False
+                    visited[candidate] = True
+                    self.backtrack_dfs(path, dest, graph, solutions, visited)
+                    path.pop()
+                    visited[candidate] = False
                     if self.finished:
                         return
 
-    def process_solutions(self):
-        pass
+    def process_solutions(
+        self, graph: Dict[str, Dict[str, float]], solutions: List[List[str]]
+    ):
+        answers: List[float] = []
+        for solution in solutions:
+            answer: float = 1.0
+            if len(solution) == 0:
+                answer = -1.0
+            for i, const in enumerate(solution):
+                if const not in graph:
+                    answer = -1.0
+                    break
+                # skip last item in list
+                if i == len(solution) - 1:
+                    break
+                next_item = solution[i + 1]
+                answer = answer * graph[const][next_item]
+            answers.append(answer)
+        return answers
 
     def calcEquation(
         self,
@@ -38,26 +65,27 @@ class Solution:
         #       'b': { 'a': 0.5, 'c': 3.0 },
         #       'c' { 'b': 0.33 }
         # }
+        graph: Dict[str, Dict[str, float]] = {}
+        visited: Dict[str, bool] = {}
         for i, val in enumerate(values):
             x, y = equations[i]
-            x_adj_list = self.graph.setdefault(x, {})
-            y_adj_list = self.graph.setdefault(y, {})
+            x_adj_list = graph.setdefault(x, {})
+            y_adj_list = graph.setdefault(y, {})
             x_adj_list[y] = val
             y_adj_list[x] = 1 / val
-            self.visited[x] = False
-            self.visited[y] = False
+            visited[x] = False
+            visited[y] = False
 
-        print(self.graph)
+        solutions: List[List[str]] = []
         for query in queries:
             source, dest = query
             path: List[str] = [source]
-            self.visited[source] = True
-            self.backtrack_dfs(path, dest)
+            visited[source] = True
+            self.backtrack_dfs(path, dest, graph, solutions, visited)
             self.finished = False
-            self.visited[source] = False
+            visited[source] = False
 
-        print(self.solutions)
-        return self.process_solutions()
+        return self.process_solutions(graph, solutions)
 
 
 class TestSolution(unittest.TestCase):
@@ -88,6 +116,27 @@ class TestSolution(unittest.TestCase):
                 [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]],
             ),
             [0.50000, 2.00000, -1.00000, -1.00000],
+        )
+        self.sol.calcEquation(
+            [["x1", "x2"], ["x2", "x3"], ["x3", "x4"], ["x4", "x5"]],
+            [3.0, 4.0, 5.0, 6.0],
+            [
+                ["x1", "x5"],
+                ["x5", "x2"],
+                ["x2", "x4"],
+                ["x2", "x2"],
+                ["x2", "x9"],
+                ["x9", "x9"],
+            ],
+        )
+        # TODO backtrack_dfs doesn't have a way of detecting no solution
+        self.assertEqual(
+            self.sol.calcEquation(
+                [["a", "b"], ["c", "d"]],
+                [1.0, 1.0],
+                [["a", "c"], ["b", "d"], ["b", "a"], ["d", "c"]],
+            ),
+            [-1.00000, -1.00000, 1.00000, 1.00000],
         )
 
 
