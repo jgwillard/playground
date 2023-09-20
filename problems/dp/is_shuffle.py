5097e1b8da9eeefd0970c077103c8cc4bad50ee2
John Willard <willard.john.gray@gmail.com>
2023-09-19 22:00:51 -0400

    implement recursive solution for is shuffle

---
+++ problems/dp/is_shuffle.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+class Solution:
+    def is_shuffle(self, x: str, y: str, z: str) -> bool:
+
+        # defines the following process for x = cat, y = dog, z = catdog
+        # s(3, 3) -> catdog, cat, dog AND z[6] == y[3]
+        # s(3, 2) -> catdo, cat, do AND z[5] == y[2]
+        # s(3, 1) -> catd, cat, d AND z[4] == y[1]
+        # s(3, 0) -> cat, cat, AND z[1..3] == x[1..3]
+        def s(i, j):
+            if i == 0:
+                return y[0:j] == z[0 : i + j]
+            if j == 0:
+                return x[0:i] == z[0 : i + j]
+            return (s(i - 1, j) and z[i + j - 1] == x[i - 1]) or (
+                s(i, j - 1) and z[i + j - 1] == y[j - 1]
+            )
+
+        return s(len(x), len(y))
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_is_shuffle(self):
+        self.assertTrue(
+            self.sol.is_shuffle("chocolate", "chips", "cchocohilaptes")
+        )
+        self.assertFalse(
+            self.sol.is_shuffle("chocolate", "chips", "chocochilatspe")
+        )
+        self.assertTrue(self.sol.is_shuffle("ss", "o", "sos"))
+        self.assertTrue(self.sol.is_shuffle("sos", "", "sos"))
+        self.assertTrue(self.sol.is_shuffle("", "sos", "sos"))
+        self.assertTrue(self.sol.is_shuffle("cat", "cat", "catcat"))
+        self.assertTrue(self.sol.is_shuffle("cat", "dog", "catdog"))
+        self.assertTrue(self.sol.is_shuffle("cat", "dog", "cadogt"))
+        self.assertTrue(self.sol.is_shuffle("cat", "dog", "cadotg"))
+        self.assertFalse(self.sol.is_shuffle("cat", "dog", "caodtg"))
+
+
+if __name__ == "__main__":
+    unittest.main()
